0e718977730f9f10bf5f254d656fdf56a7d531dd
FujishimaGitKatsuya <fujikatz@gmail.com>
2025-05-19 05:43:43 +0900

    Update event.js

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -6,6 +6,5 @@ btn.addEventListener('click', () => {
 // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
 setTimeout(() => {
   text.textContent = 'ボタンをクリックしました';
-        })
-        
-      }, 2000);
+}, 2000);
+ });
